a2761005ef3f04b777c6f10d25e79c3110438fde
守屋千春 <mry116k@hotmail.com>
2018-01-02 18:57:24 +0900

    xml2jsを入れ、rssデータを受信する

--- routes/hello.js
+++ routes/hello.js
@@ -1,32 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+//RSS受信モジュール追記
+var http = require('https');
+var parseString = require('xml2js').parseString;
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var msg = '※何か書いて送信してください。';
-  //セッションの値の有無を確認
-  if(req.session.message != undefined){
-    msg = "Last Message : " + req.session.message;
-  }
-  var data = {
-    title: 'Hello!',
-    content: msg
+  var opt = {
+    host: 'news.google.com',
+    port: 443,
+    path: '/news?hl=ja&ned=us&ie=UTF-8&oe=UTF-8&output=rss'
   };
-  res.render('hello', data);
-});
 
-//ポスト受信
-router.post('/post', function(req, res, next) {
-  //ポスト取得　input name='message'
-  var msg = req.body['message'];
-  //セッションに入れる
-  req.session.message = msg;
-  //表示
-  var data = {
-    title: 'Hello!',
-    content: "Last Message : " + req.session.message
-  };
-  res.render('hello', data);
+  http.get(opt,(rss2) => {
+    var body = '';
+
+    rss2.on('data',(data) => {
+      body += data;
+    });
+
+    rss2.on('end',() => {
+      parseString(body.trim(), (err, result) => {
+        var data = {
+          title: 'Hello',
+          content: result.rss.channel[0].item
+        };
+        res.render('hello', data);
+      });
+    });
+  });
 });
 
 module.exports = router;
