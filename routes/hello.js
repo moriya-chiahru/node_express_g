038b2f1bb8fad22fe6a1febbe970378edda78414
守屋千春 <mry116k@hotmail.com>
2018-01-02 17:07:24 +0900

    クエリの取得

--- routes/hello.js
+++ routes/hello.js
@@ -3,9 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //クエリの取得
+  var name = req.query.name;
+  var mail = req.query.mail;
+  //表示
   var data = {
-    title: 'hello!',
-    content: 'これはサンプルのコンテンツです。'
+    title: 'Hello!',
+    content: 'あなたの名前は' + name + 'にゃ。<br>' +
+    'メールアドレスは' + mail + 'だにゃ！'
   };
   res.render('hello', data);
 });
