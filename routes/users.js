42785d37578e9f0a06db19414614997ee52b125f
守屋千春 <mry116k@hotmail.com>
2018-01-03 11:43:19 +0900

    user情報を表示する

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  res.json([
+    {id: 1, username: "somebody"},
+    {id: 2, username: "somebody_else"}
+    ]);
 });
 
 module.exports = router;
